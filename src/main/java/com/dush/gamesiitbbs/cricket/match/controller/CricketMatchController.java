3770155aa2e3c9d9bdad62298ceca407bb5f86ea
dushyanth <kd13@iitbbs.ac.in>
2022-01-10 19:10:08 +0530

    cricket init

--- src/main/java/com/dush/gamesiitbbs/cricket/match/controller/CricketMatchController.java
+++ src/main/java/com/dush/gamesiitbbs/cricket/match/controller/CricketMatchController.java
@@ -2,8 +2,6 @@ package com.dush.gamesiitbbs.cricket.match.controller;
 
 import java.util.Optional;
 
-import javax.annotation.PostConstruct;
-
 import com.dush.gamesiitbbs.cricket.match.model.CricketMatch;
 import com.dush.gamesiitbbs.cricket.match.service.CricketMatchService;
 
@@ -12,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/dush/gamesiitbbs/football/match/controller/FootballMatchController.java
+++ src/main/java/com/dush/gamesiitbbs/football/match/controller/FootballMatchController.java
@@ -2,8 +2,6 @@ package com.dush.gamesiitbbs.football.match.controller;
 
 import java.util.Optional;
 
-import javax.annotation.PostConstruct;
-
 import com.dush.gamesiitbbs.football.match.model.FootballMatch;
 import com.dush.gamesiitbbs.football.match.service.FootballMatchService;
 
@@ -12,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
